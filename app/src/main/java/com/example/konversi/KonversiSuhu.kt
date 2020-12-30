a65baac2a833cef48322ae611100546fd8de55eb
dendisantoso74 <dendi.santoso74@gmail.com>
2020-12-30 16:52:38 +0700

    konversi suhu

--- app/src/main/java/com/example/konversi/KonversiSuhu.kt
+++ app/src/main/java/com/example/konversi/KonversiSuhu.kt
@@ -1,11 +1,45 @@
 package com.example.konversi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class KonversiSuhu : AppCompatActivity() {
+
+    var celcius: EditText? = null
+    var kelvin: EditText? = null
+    var farenheit: EditText? = null
+    var reamur: EditText? = null
+    var btn_konversi: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_konversi_suhu)
+
+        celcius = findViewById<View>(R.id.edtxt_celcius) as EditText
+        kelvin = findViewById<View>(R.id.edtxt_kelvin) as EditText
+        farenheit = findViewById<View>(R.id.edtxt_farenheit) as EditText
+        reamur = findViewById<View>(R.id.edtxt_reamur) as EditText
+        btn_konversi = findViewById<View>(R.id.btn_konvert) as Button
+    }
+
+    fun konversisuhu(view: View?) {
+        try {
+            val ncelcius = celcius!!.text.toString().toInt()
+            val nkelvin = ncelcius + 273.15
+            val nfarenheit = 1.8*ncelcius+32
+            val nreamur = 0.8*ncelcius
+            kelvin!!.setText(nkelvin.toString())
+            farenheit!!.setText(nfarenheit.toString())
+            reamur!!.setText(nreamur.toString())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun keluar(view: View?) {
+        finish()
     }
 }
